20b2fcf2ac3149db67b8ed808ccd0f0ba88f73a5
namji <skawlgns07@naver.com>
2024-02-21 13:33:26 +0900

    init commit

---
+++ src/test/java/com/namji/mytodo/MytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namji.mytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
