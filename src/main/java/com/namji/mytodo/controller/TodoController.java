8a4b90a3013f933e009358651823d29c82078cfe
namji <skawlgns07@naver.com>
2024-02-21 16:02:44 +0900

    feat: My Todo Project 할일 전체 조회 기능 구현
    할일 생성 후 생성된 데이터 전체 조회 기능 구현

--- src/main/java/com/namji/mytodo/controller/TodoController.java
+++ src/main/java/com/namji/mytodo/controller/TodoController.java
@@ -2,14 +2,14 @@ package com.namji.mytodo.controller;
 
 import com.namji.mytodo.dto.todo.requestDto.TodoRequestDto;
 import com.namji.mytodo.dto.todo.responseDto.TodoResponseDto;
+import com.namji.mytodo.entity.Todo;
 import com.namji.mytodo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,13 @@ public class TodoController {
         TodoResponseDto responseDto = todoService.createTodo(requestDto);
 
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    // 할일 전체 조회
+    @GetMapping("/todoList")
+    public ResponseEntity<List<Todo>> getTodoList() {
+        List<Todo> todoList = todoService.getTodoList();
+
+        return new ResponseEntity<>(todoList, HttpStatus.OK);
     }
 }
--- src/main/java/com/namji/mytodo/service/TodoService.java
+++ src/main/java/com/namji/mytodo/service/TodoService.java
@@ -5,7 +5,11 @@ import com.namji.mytodo.dto.todo.responseDto.TodoResponseDto;
 import com.namji.mytodo.entity.Todo;
 import com.namji.mytodo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.NewConstructorTypeMunger;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +24,12 @@ public class TodoService {
 
         return new TodoResponseDto(todo.getTitle(), todo.getContents());
         // todo: DB의 값을 가져와서 반환하는게 아닌 내가 입력한 값을 반환 -> 저장하고 DB 값 가져오는 방법은?
+    }
+
+    public List<Todo> getTodoList() {
+        List<Todo> todo = todoRepository.findAll();
+
+        return todo;
+        // todo: 전체 조회에서 엔티티를 반환하지 않는 방법? ResponseDto 사용해서 반환하는 방법
     }
 }
